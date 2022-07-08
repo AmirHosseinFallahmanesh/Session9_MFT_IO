54232c9b4ba737189a2daa3e01a191883d625580
AmirHosseinFallahmanesh <54832303+AmirHosseinFallahmanesh@users.noreply.github.com>
2022-07-08 11:46:53 +0430

    test

---
+++ Part4/Program.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+
+namespace Part4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WebRequest request = WebRequest.Create("https://jsonplaceholder.typicode.com/todos");
+            WebResponse response = request.GetResponse();
+            Stream stream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(stream);
+            string json = reader.ReadToEnd();
+            var resut= JsonConvert.DeserializeObject<List<UserModel>>(json);
+            foreach (var item in resut)
+            {
+                Console.WriteLine(item.Title);
+                Console.WriteLine(item.Completed);
+                Console.WriteLine("***********************");
+            }
+            Console.ReadKey();
+        }
+    }
+    public class UserModel {
+
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string Title { get; set; }
+        public bool Completed { get; set; }
+
+    }
+
+}
