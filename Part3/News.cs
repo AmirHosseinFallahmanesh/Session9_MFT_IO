1e1c60d5c699f659aaee3fa5287d5afb47a68265
AmirHosseinFallahmanesh <54832303+AmirHosseinFallahmanesh@users.noreply.github.com>
2022-07-08 10:49:21 +0430

    test

---
+++ Part3/News.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Part3
+{
+    public class News
+    {
+        public News()
+        {
+            Id = Guid.NewGuid();
+        }
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public DateTime PubDate { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ Part3/Program.cs
@@ -0,0 +1,76 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace Part3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader reader = new StreamReader("news.txt");
+            string data = reader.ReadToEnd();
+            List<News> news= JsonConvert.DeserializeObject<List<News>>(data);
+            reader.Close();
+            Console.WriteLine("Done");
+
+
+
+        }
+
+        private static void JsonSerial()
+        {
+            List<News> newsList = new List<News>();
+            newsList.Add(new News()
+            {
+                PubDate = new DateTime(2020, 5, 5),
+                Title = "ایران برد",
+                Description = "ایران لهستان را برد"
+            });
+            newsList.Add(new News()
+            {
+                PubDate = new DateTime(2020, 5, 6),
+                Title = "ایران باخت",
+                Description = "ایران از برزیل باخت"
+            });
+
+            string data = JsonConvert.SerializeObject(newsList);
+            StreamWriter writer = new StreamWriter("news.txt");
+            writer.Write(data);
+            writer.Flush();
+            writer.Close();
+        }
+
+        private static void XmlDeserial()
+        {
+            FileStream fileStream = new FileStream("news.xml", FileMode.Open);
+            XmlSerializer serializer = new XmlSerializer(typeof(List<News>));
+            List<News> news = serializer.Deserialize(fileStream) as List<News>;
+            fileStream.Close();
+        }
+
+        private static void XmlSerial()
+        {
+            List<News> newsList = new List<News>();
+            newsList.Add(new News()
+            {
+                PubDate = new DateTime(2020, 5, 5),
+                Title = "ایران برد",
+                Description = "ایران لهستان را برد"
+            });
+            newsList.Add(new News()
+            {
+                PubDate = new DateTime(2020, 5, 6),
+                Title = "ایران باخت",
+                Description = "ایران از برزیل باخت"
+            });
+
+            FileStream fileStream = new FileStream("news.xml", FileMode.OpenOrCreate);
+            XmlSerializer serializer = new XmlSerializer(typeof(List<News>));
+            serializer.Serialize(fileStream, newsList);
+            fileStream.Close();
+        }
+    }
+}
